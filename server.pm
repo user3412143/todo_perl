76c21fb8f82748c5a8ba3d97470e28479ce86dd5
user <not@now.com>
2024-02-08 09:25:12 +0500

    Add endpoints 	изменено:      index.html 	изменено:      server.pm

--- server.pm
+++ server.pm
@@ -9,36 +9,52 @@ use base qw/HTTP::Server::Simple::CGI/;
 sub handle_request {
 	my ($self, $cgi) = @_;
 
-	if ($cgi->param('add'))  {
-		my $task = $cgi->param('task');
-		if (length $task >= 4) {
-			&db_work::add_task("false", $task);
-			print "HTTP/1.1 302 Found\r\n";
-			print "Location: /\r\n";
-			print "\r\n";
-		}
-	}
-	elsif ($cgi->param('delete'))  {
-		my $id = $cgi->param('index');
-		&db_work::remove_task($id);
-	}
-	print "HTTP/1.0 200 OK\r\n";
-	print $cgi->header('text/html');
-	my $html = read_html_file("index.html");
-	my %task_hash = &db_work::get_tasks();
-	my $task_list = "";
-	my @sorted_keys = sort {$a <=> $b} keys %task_hash;
+	my $path = $cgi->path_info();
 
-	if (@sorted_keys) {
-		foreach my $id (@sorted_keys) {
-			my $task_description = $task_hash{$id};
-			$task_list .= "<li> $id: $task_description <a href='/?delete=1&index=$id'>[Delete]</a></li><p>";
+	# Endpoints
+	if ($path eq '/tasks') {
+		if ($cgi->request_method() eq 'POST')  {
+			my $task = $cgi->param('task');
+			if (length $task >= 4) {
+				&db_work::add_task("false", $task);
+				print "HTTP/1.1 302 Found\r\n";
+				print "Location: /\r\n";
+				print "\r\n";
+			}
 		}
-	} else {
-		print "No tasks was found";
+		if ($cgi->request_method() eq 'DELETE') {
+			my $id = $cgi->param('id');
+			&db_work::remove_task($id);
+		}
+		if ($cgi->request_method() eq 'GET') {
+			my %task_hash = &db_work::get_tasks();
+			my @tasks = sort {$a <=> $b} values %task_hash;
+			for my $task (@tasks) {
+				print $task . "\n";
+			}
+		}
 	}
-	$html =~s|<body>|<body>$task_list|;
-	print $html;
+	# an html page who will be showed with default view
+	if ($path eq '/') {
+		print "HTTP/1.0 200 OK\r\n";
+		print $cgi->header('text/html');
+		my $html = read_html_file("index.html");
+		my %task_hash = &db_work::get_tasks();
+		my $task_list = "";
+		my @sorted_keys = sort {$a <=> $b} keys %task_hash;
+		
+		if (@sorted_keys) {
+			foreach my $id (@sorted_keys) {
+				my $task_description = $task_hash{$id};
+				$task_list .= "<li>$id: $task_description
+				<a href='#' onclick='delete_task(event, $id)'>[Delete]</a></li><p>";
+			}
+		} else {
+			print "No tasks was found";
+		}
+		$html =~s|<body>|<body>$task_list|;
+		print $html;
+	}
 }
 sub read_html_file {
         my ($filepath) = shift;
